e111e49587cf1eb9308731c76b8047f7de66e58c
SerenaDP <48323986+dipedeserena@users.noreply.github.com>
2021-03-21 16:09:30 +0100

    Update dispatcher.py

--- dispatcher/dispatcher.py
+++ dispatcher/dispatcher.py
@@ -1,16 +1,23 @@
-#!/daq_common/miniconda3/bin/python3
 import configparser
 import argparse
+import logging
+import logging.handlers
 import threading
+import signal
 import datetime
-import os
-import daqnt
-import json
-from pymongo import MongoClient
-from urllib.parse import quote_plus
 
 from MongoConnect import MongoConnect
-from DAQController import DAQController
+from DAQController import DAQController, STATUS
+
+
+class SignalHandler(object):
+    def __init__(self):
+        self.event = threading.Event()
+        signal.signal(signal.SIGINT, self.interrupt)
+        signal.signal(signal.SIGTERM, self.interrupt)
+
+    def interrupt(self, *args):
+        self.event.set()
 
 
 def main():
@@ -21,55 +28,64 @@ def main():
             default='config.ini')
     parser.add_argument('--log', type=str, help='Logging level', default='DEBUG',
             choices=['DEBUG','INFO','WARNING','ERROR','CRITICAL'])
-    parser.add_argument('--test', action='store_true', help='Are you testing?')
     args = parser.parse_args()
     config = configparser.ConfigParser()
     config.read(args.config)
-    config = config['DEFAULT' if not args.test else "TESTING"]
-    config['MasterDAQConfig'] = json.loads(config['MasterDAQConfig'])
-    control_mc = daqnt.get_client('daq')
-    runs_mc = daqnt.get_client('runs')
-    logger = daqnt.get_daq_logger(config['LogName'], level=args.log, mc=control_mc)
-    vme_config = json.loads(config['VMEConfig'])
+    
+    # Logger
+    logger = logging.getLogger('main')
+    # Stream Logger
+    h = logging.StreamHandler()    
+    f = logging.Formatter(fmt='%(asctime)s | %(levelname)s | %(message)s')
+    h.setFormatter(f)
+    logger.addHandler(h)
+    # File Handler Logger
+    h = logging.handlers.TimedRotatingFileHandler('Log_files/log_', when='midnight', utc=True, backupCount=7) 
+    h.setFormatter(f) 
+    logger.addHandler(h)
+    # Logger levels
+    logger.setLevel(getattr(logging, args.log))
+    
+    # Declare database object
+    MongoConnector = MongoConnect(config, logger)
 
-    # Declare necessary classes
-    sh = daqnt.SignalHandler()
-    Hypervisor = daqnt.Hypervisor(control_mc[config['ControlDatabaseName']], logger,
-            config['MasterDAQConfig']['tpc'], vme_config, sh=sh, testing=args.test)
-    MongoConnector = MongoConnect(config, logger, control_mc, runs_mc, Hypervisor, args.test)
-    DAQControl = DAQController(config, MongoConnector, logger, Hypervisor)
-
-    sleep_period = int(config['PollFrequency'])
-
-    logger.info('Dispatcher starting up')
+    # Declare a 'brain' object. This will cache info about the DAQ state in order to
+    # solve the want/have decisions. The deciding functions also go here. It needs a
+    # mongo connector because it has to pull options files
+    DAQControl = DAQController(config, MongoConnector, logger)
+    sleep_period = int(config['DEFAULT']['PollFrequency'])
+    sh = SignalHandler()
 
     while(sh.event.is_set() == False):
+        
         sh.event.wait(sleep_period)
+
         # Get most recent check-in from all connected hosts
-        if MongoConnector.get_update():
+        if MongoConnector.GetUpdate():
             continue
         latest_status = MongoConnector.latest_status
 
         # Get most recent goal state from database. Users will update this from the website.
-        if (goal_state := MongoConnector.get_wanted_state()) is None:
+        goal_state = MongoConnector.GetWantedState()
+        if goal_state is None:
             continue
 
         # Print an update
         for detector in latest_status.keys():
             state = 'ACTIVE' if goal_state[detector]['active'] == 'true' else 'INACTIVE'
             msg = (f'The {detector} should be {state} and is '
-                    f'{latest_status[detector]["status"].name}')
+                  f'{latest_status[detector]["status"].name}')
+            
             if latest_status[detector]['number'] != -1:
-                msg += f' ({latest_status[detector]["number"]})'
+                msg += f'({latest_status[detector]["number"]})'
             logger.debug(msg)
 
         # Decision time. Are we actually in our goal state? If not what should we do?
-        DAQControl.solve_problem(latest_status, goal_state)
+        DAQControl.SolveProblem(latest_status, goal_state)
 
         # Time to report back
-        MongoConnector.update_aggregate_status()
-
-    MongoConnector.quit()
+        MongoConnector.UpdateAggregateStatus()
+    MongoConnector.Quit()
     return
 
 
