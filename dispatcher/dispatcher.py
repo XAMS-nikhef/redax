014d977552fb2ea48d9c5b67e7932bb2b6b47dd2
xams-daq <xamsemergency@gmail.com>
2021-08-06 10:40:01 +0200

    No need for HV now

--- dispatcher/dispatcher.py
+++ dispatcher/dispatcher.py
@@ -33,14 +33,9 @@ def main():
 
     # Declare necessary classes
     sh = daqnt.SignalHandler()
-#     SlackBot = daqnt.DaqntBot(os.environ['SLACK_KEY'])
-    Hypervisor = daqnt.Hypervisor(control_mc[config['ControlDatabaseName']], logger,
-            daq_config['tpc'], vme_config, sh=sh, testing=args.test)
-    MongoConnector = MongoConnect(config, daq_config, logger, control_mc, runs_mc, Hypervisor, args.test)
-    DAQControl = DAQController(config, daq_config, MongoConnector, logger, Hypervisor)
+    MongoConnector = MongoConnect(config, daq_config, logger, control_mc, runs_mc, args.test)
+    DAQControl = DAQController(config, daq_config, MongoConnector, logger)
     # connect the triangle
-    Hypervisor.mongo_connect = MongoConnector
-    Hypervisor.daq_controller = DAQControl
 
     sleep_period = int(config['PollFrequency'])
 
