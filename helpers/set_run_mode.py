6f4182487254628214d79ccc211df0ca02b066c0
xams-daq <xamsemergency@gmail.com>
2021-11-15 23:08:05 +0100

    add some other helpers

--- helpers/set_run_mode.py
+++ helpers/set_run_mode.py
@@ -4,111 +4,50 @@ from bson.objectid import ObjectId
 import os
 
 
-
-client = MongoClient("mongodb://daq:%s@localhost:27017/admin"%os.environ["MONGO_PASSWORD"])
-#uri = "mongodb://admin:%s@127.0.0.1:27017/admin"%os.environ["MONGO_PASSWORD"]
-#client = pymongo.MongoClient(uri)
-db = client['xenonnt']
+client = MongoClient("mongodb://user:%s@127.0.0.1:27017/admin"%os.environ["MONGO_PASSWORD"])
+db = client['daq']
 collection = db['options']
 
 run_mode = {
-	"_id": ObjectId("5d30e9d05e13ab6116c43bf9"),
-    "name": "default_firmware_settings",
-    "user": "zhut",
-    "description": "Setup for default firmware",
-    "detector" : "NaI",
-    "mongo_uri": "mongodb://daq:%s@localhost:27017/admin"%os.environ["MONGO_PASSWORD"],
-    "mongo_database": "xenonnt",
-    "mongo_collection": "test_NaI",
+	# Organitation Fields
+	# "_id": ObjectId("5d30e9d05e13ab6116c43bf9"),
+    "name": "software_update_V1730_part2",
+    "user": "dipede",
+    "description": "redax update for V1730",
+    "detector" : "xams",
+    # "mongo_uri": "mongodb://user:%s@127.0.0.1:27017/admin"%os.environ["MONGO_PASSWORD"],
+    # "mongo_database": "daq",
+    # "mongo_collection": "test_NaI",
     "run_start":0,
-    "strax_chunk_overlap": 500000000,
+    "strax_chunk_overlap": 0.5,# 500000000,
     "strax_header_size": 31,
-    "strax_output_path": "/home/zhut/raw",
-    "strax_chunk_length": 5000000000,
+    "strax_output_path": "/home/xams/daq/redax/data",
+    "strax_chunk_length": 5, #5000000000,
     "strax_fragment_length": 220,
-    "baseline_dac_mode": "fit",
+    "baseline_dac_mode": "fixed",
     "baseline_value": 16000,
-    "firmware_version": 1,
-    "boards":
-    [
-        {"crate": 0, "link": 4, "board": 165,
-            "vme_address": "FFFF0000", "type": "V1724", "host": "fdaq00_reader_7"},
-    ],
-    "registers" : [
-		{
-			"comment" : "board reset register",
-			"board" : -1,
-			"reg" : "EF24",
-			"val" : "0"
-		},
-		{
-			"comment" : "events per BLT-originally it was 1",
-			"board" : -1,
-			"reg" : "EF1C",
-			"val" : "1"
-		},
-		{
-			"comment" : "Front Panel Trigger Out Enable Mask",
-			"board" : -1,
-			"reg" : "8110",
-			"val" : "00000000"
-		},
-		{
-			"comment" : "for  BUSY on TRG_OUT, LVDS new management, General Purpose I/O, LVDS[15-4] outputs, LVDS[3-0] inputs, TRG/CLK at TTL level (139) or NIM level (138)",
-			"board" : -1,
-			"reg" : "811C",
-			"val" : "D0138"
-		},
-		{
-			"comment" : "BERR register, 10=enable BERR",
-			"board" : -1,
-			"reg" : "EF00",
-			"val" : "10"
-		},
-		{
-			"comment" : "Trigger logic: 80000000 is software only, software + external C0000000",
-			"board" : -1,
-			"reg" : "810C",
-			"val" : "C0000000"
-		},
-		{
-			"comment" : "Post Trigger (time between trigger and end of time window). 80 for 2.5 us",
-			"board" : -1,
-			"reg" : "8114",
-			"val" : "80"
-		},
-		{
-			"comment" : "Channel enable mask. FF= all channels on",
-			"board" : -1,
-			"reg" : "8120",
-			"val" : "FF"
-		},
-		{
-			"comment" : "Buffer organization register. A for new FW.",
-			"board" : -1,
-			"reg" : "800C",
-			"val" : "A"
-		},
-		{
-			"comment" : "DAC default configuration. 1000 = neg unipolar + offset, 1000",
-			"board" : -1,
-			"reg" : "8098",
-			"val" : "1000"
-		},
-		{
-			"comment" : "50: NO ZS + Falling + Sequential + External signal + Non Overlap Triggers, 20050 same with ZLE",
-			"board" : -1,
-			"reg" : "8000",
-			"val" : "50"
-		},
-		{
-			"comment" : "Event size register. Required for new FW. Words.",
-			"board" : -1,
-			"reg" : "8020",
-			"val" : "0"
-		},
+	"baseline_fixed_value":4000,
+    "firmware_version": 0,
+	"boards" : [{"crate": 1,
+                "link": 0,
+               "board": 770,
+               "vme_address": "0",
+               "type": "V1724",
+               "host": "xams-daq_reader_0"}],
+    "registers": [{'reg': 'EF24', 'val': '1','board': 770,'comment': 'Software reset'},
+                  {'reg': 'EF1C','val': 'FF','board': 770, 'comment': 'Max number of events per blt'},
+                  {'reg': 'EF00', 'val': '10', 'board': 770, 'comment': 'Readout control'},
+                  {'reg': '8120', 'val': 'FF', 'board': 770, 'comment': 'Channel mask'},
+                  {'reg': '8000','val': '310','board': 770,'comment': 'Board configuration'},
+                  {'reg': '8038','val': '19','board': 770,'comment': 'Word in pretrigger window'},
+                  {'reg': '8020','val': '32','board': 770,'comment': 'Event size register. Required for new FW. Words.'},
+                  {'reg': '8078','val': '19','board': 770,'comment': 'Samples under threshold to close event.'},
+                  {'reg': '8060', 'val': '12', 'board': 770, 'comment': 'Trigger threshold. '},
+                  {'reg': '8080','val': '510000','board': 770,'comment': 'DPP register. 310000 - DPP on, 256 sample baseline.'}
 	],
-    "channels":{"165":[0, 1, 2, 3, 4, 5, 6, 7]},
+    'channels':{'770': [0, 1, 2, 3, 4, 5, 6, 7]},
+    'thresholds': {'770': [100, 100, 100, 100, 100, 100, 100, 100]},
+
 }
 
 if collection.find_one({"name": run_mode['name']}) is not None:
