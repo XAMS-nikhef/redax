8b2ac5bd40d195792409478cb4d0561e98f6522e
acolijn <auke.pieter.colijn@gmail.com>
2024-11-01 11:43:28 +0100

    Fix read_reg function to our adapter!

--- read_reg.cpp
+++ read_reg.cpp
@@ -7,16 +7,17 @@
 int main(int argc, char** argv){
 
   if (argc != 5 && argc != 6) {
-    std::cout<<"Usage: " << argv[0] << " link crate <r|w> register [value]\n";
+    std::cout<<"Usage: " << argv[0] << " link link_PID <r|w> register [value]\n";
     return 1;
   }
+  
   int ret;
-  CVBoardTypes BType = cvV2718;
-  int handle = -1;
+  CVBoardTypes BType = cvUSB_A4818_V3718_LOCAL;
+  int fBoardHandle = -1;
   int link = atoi(argv[1]);
-  int crate = atoi(argv[2]);
+  int link_PID = atoi(argv[2]);
   char op = argv[3][0];
-  ret = CAENVME_Init(BType, link, crate, &handle);
+  ret = CAENVME_Init2(BType, &link_PID, 0, &fBoardHandle);
   if(ret != cvSuccess){
     std::cout<<"Failed to initialize digitizer: "<<ret<<std::endl;
     exit(0);
@@ -33,19 +34,19 @@ int main(int argc, char** argv){
   std::stringstream s(argv[4]);
   s >> std::hex >> reg;
   if (op == 'r') {
-    ret = CAENVME_ReadCycle(handle,reg,&val,cvA32_U_DATA,cvD32);
+    ret = CAENVME_ReadCycle(fBoardHandle,reg,&val,cvA32_U_DATA,cvD32);
     if(ret != cvSuccess){
       std::cout<<"Failed to read 0x"<<std::hex<<reg<<std::dec<<": "<<ret<<std::endl;
     }
     std::cout<<"Read 0x" <<std::hex<<reg<<" as 0x"<<val<<std::dec<<std::endl;
   } else if (op == 'w') {
-    ret = CAENVME_WriteCycle(handle, reg, &val, cvA32_U_DATA, cvD32);
+    ret = CAENVME_WriteCycle(fBoardHandle, reg, &val, cvA32_U_DATA, cvD32);
     if(ret != cvSuccess){
       std::cout<<"Failed to write to 0x"<<std::hex<<reg<<std::dec<<": "<<ret<<std::endl;
     }
     std::cout<<"Wrote to 0x" <<std::hex<<reg<<std::dec<<std::endl;
   }
 
-  CAENVME_End(handle);
+  CAENVME_End(fBoardHandle);
   return 0;
 }
